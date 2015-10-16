a6674030885a0b770d53af223a981c80b0d887ff
nirleibo <nir.leibo@gmail.com>
2015-10-16 10:15:08 +0300

    - create basic project layout by mvp design

---
+++ server/maze server/src/model/CommonModel.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.Observable;
+
+public abstract class CommonModel extends Observable implements Model {
+
+}
---
+++ server/maze server/src/presenter/CommonPresenter.java
@@ -0,0 +1,5 @@
+package presenter;
+
+public abstract class CommonPresenter {
+
+}
